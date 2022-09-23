1cc14c86cf4243099ebb7d16d34ea387d3ae7de6
Ashlynn223 <ashlynnmitchell223@gmail.com>
2022-09-23 16:05:56 -0700

    Change header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Calendar!</h1>
     </div>
   );
 }
